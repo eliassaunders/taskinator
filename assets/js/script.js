a620c671ad3b25956237ed86d6bb67d0f55591ae
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-01 15:28:50 -0700

    blank tasks fixed and promt reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,15 +8,21 @@ var taskFormHandler = function (event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type'").value;
 
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to full out the task form!");
+    return false;
+  }
+formEl.reset();
   var taskDataObj = {
     name: taskNameInput,
     type: taskTypeInput
-  };
 
+  };
+  
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskDataObj) {
+var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
